9555ea2950dc083ab14c209021d7370192171d9b
rafiul1000 <rafiulislam5075@gmail.com>
2023-09-10 12:03:37 +0600

    Updating state in arrays and pass parameter

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -9,7 +9,7 @@ const Country = ({country, handleVisitedCountry}) => {
         setVisited(!visited);
     }
 
-    console.log(handleVisitedCountry);
+
 
 
     return (
@@ -19,7 +19,7 @@ const Country = ({country, handleVisitedCountry}) => {
             <p>Population: {population}</p>
             <p>Area: {area}</p>
             <p><small>Code: {cca3}</small></p>
-            <button>Mark visited</button>
+            <button onClick={() => handleVisitedCountry(country)}>Mark visited</button>
             <br />
             <button onClick={handleVisited}>{visited ? 'Visited' : 'Going'}</button>
             {visited ? 'I have visited this country.' : 'I want to visit.'}
